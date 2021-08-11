32dc86afd21040b7243aed2da00bc2a278572a0f
ninsbl <stefan.blumentrath@gmx.de>
2021-08-11 23:52:15 +0200

    workflow

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 
-set PATH=c:/OSGeo4W/bin
+set PATH=c:/OSGeo4W/bin;C:\msys64/usr/bin/
 
-dir c:/OSGeo4W/bin;C:\msys64/usr/bin/
+dir c:/OSGeo4W/bin
 
 echo test_call_sh.py
 c:/OSGeo4W/bin/python3 test_call_sh.py
