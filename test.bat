eee23f61923f1abc7962a6008ac6e87080cdea4e
ninsbl <stefan.blumentrath@gmx.de>
2021-08-11 23:47:57 +0200

    workflow

--- test.bat
+++ test.bat
@@ -3,6 +3,10 @@ set PATH=c:/OSGeo4W/bin
 
 dir c:/OSGeo4W/bin
 
+echo test_call_sh.py
+c:/OSGeo4W/bin/python3 test_call_sh.py
+echo test.sh
+bash test.sh
 echo test.py
 c:/OSGeo4W/bin/python3 test.py
 echo test3.py
