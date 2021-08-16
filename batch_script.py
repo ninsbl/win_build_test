3907ba86f2e69c5891fd2cbeb64c73df46de40d0
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-08-16 22:57:08 +0200

    Update batch_script.py

--- batch_script.py
+++ batch_script.py
@@ -2,7 +2,30 @@
 
 import os
 import sys
+import argparse
 
-print(os.environ["PATH"])
-print(sys.executable)
-print(sys.version)
+
+def main():
+    print([args.path] * 200)
+    print(os.environ["PATH"])
+    print(sys.executable)
+    print(sys.version)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Import data from a file (or files) on an istSOS server.')
+
+    parser.add_argument(
+        '-path',
+        type=str,
+        dest='path',
+        required=True,
+        help='Path to a file with observations '
+             '(only working directory with files when using -d flag)')
+
+
+    args = parser.parse_args()
+
+    main()
