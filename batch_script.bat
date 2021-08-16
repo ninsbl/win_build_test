b62223dfea04f4e251f7b3bbffcb641e60f9d6ac
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-08-16 23:05:18 +0200

    Update batch_script.bat

--- batch_script.bat
+++ batch_script.bat
@@ -1,13 +1,5 @@
 
 set PATH=c:/OSGeo4W/bin;C:\msys64/usr/bin/
 
-echo %PATH%
-dir c:/OSGeo4W/bin
-
-echo test_call_sh.py
-c:/OSGeo4W/bin/python3 test_call_sh.py
-
 echo python_script.py
-c:/OSGeo4W/bin/python3 python_script.py
-echo python_script3.py
-c:/OSGeo4W/bin/python3 python_script3.py
+c:/OSGeo4W/bin/python3 batch_script.py
