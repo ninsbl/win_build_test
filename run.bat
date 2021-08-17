78bd72045807bc4bc0d4f6d9ff4a3d6ef6e22129
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-08-17 09:20:54 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-set PATH=C:/OSGeo4W/bin;C:/Program Files/Git/mingw64/bin;C:/Program Files/Git/usr/bin
+set PATH=C:/OSGeo4W/bin;C:/Program Files/Git/mingw64/bin;C:/Program Files/Git/usr/bin;./
 
 C:/osgeo4w/bin/python3.exe test_call_sh.py
