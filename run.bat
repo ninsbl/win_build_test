8e98e257d3103987aa99acf34a2c203d353e524a
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-08-25 12:04:37 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,12 +8,13 @@ rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxil
 rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/include"
 rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/lib"
 rem dir "C:/MSYS64/MinGW64"
-rem dir "C:/MSYS64/MinGW64/lib/gcc/"
+dir "C:/MSYS64/MinGW64/lib/gcc/"
 dir "C:/MSYS64/MinGW64/lib/gcc/x86_64-w64-mingw32/10.3.0"
+dir "C:/MSYS64/MinGW64/lib/gcc/x86_64-w64-mingw32/10.3.0/include"
 rem dir "C:/MSYS64/MinGW64/lib/gcc/include/c++"
 rem dir "C:/MSYS64/MinGW64/lib/gcc/include/c++/4.9.2"
-dir "C:/MSYS64/MinGW64/include/c++"
-rem dir "C:/MSYS64/MinGW64/x86_64-w64-mingw32/10.3.0"
+dir "C:/MSYS64/MinGW64/include/c++/10.3.0"
+rem dir "C:/MSYS64/MinGW64/x86_64-w64-mingw32"
 
 echo %PATH%
 
