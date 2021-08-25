395d497b7c9d172a7aadf3c75a1c6b23a654a646
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-08-25 08:19:04 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
 set PATH=C:/OSGeo4W/bin;C:/Program Files/Git/mingw64/bin;C:/Program Files/Git/usr/bin;./
 
+dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise"
+dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC"
+dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/bin"
+
 echo %PATH%
 
 C:/osgeo4w/bin/python3.exe test_call_sh.py
