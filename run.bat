ac1c4f1a1fae568f71f9ae9f20006e177829f050
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-08-25 09:32:58 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,14 +1,16 @@
 set PATH=C:/OSGeo4W/bin;C:/Program Files/Git/mingw64/bin;C:/Program Files/Git/usr/bin;./
 
-dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/Common7/Tools"
-dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/SDK"
-dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
-dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/VS/include"
-dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/VS/lib/x64"
-dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/include"
-dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/lib"
+rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/Common7/Tools"
+rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/SDK"
+rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
+rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/VS/include"
+rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/VS/lib/x64"
+rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/include"
+rem dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/lib"
 dir "C:/MSYS64/MinGW64"
-dir "C:/MSYS64/MinGW64/gcc"
+dir "C:/MSYS64/MinGW64/lib"
+dir "C:/MSYS64/MinGW64/include"
+dir "C:/MSYS64/MinGW64/x86_64-w64-mingw32"
 
 echo %PATH%
 
