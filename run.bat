d08e6c96360d3afb5931d169c42e9097095e3a6a
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-09-08 08:34:49 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,11 +1,12 @@
 set PATH=C:/OSGeo4W/bin;C:/Program Files/Git/mingw64/bin;C:/Program Files/Git/usr/bin;./
-dir C:/Program Files (x86)/Windows Kits
-dir C:/Program Files (x86)/Windows Kits/10
-dir C:/Program Files (x86)/Windows Kits/10/Include
+dir "C:/Program Files (x86)/Windows Kits"
+dir "C:/Program Files (x86)/Windows Kits/10"
+dir "C:/Program Files (x86)/Windows Kits/10/Include"
 dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/Common7/Tools"
 dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/SDK"
 call "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
-reg query HKCU\Environment
+cd "C:/Program Files (x86)"
+dir /s *foo* corecrt_terminate.h
 dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/VS/include"
 dir "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/VS/lib/x64"
 call "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvarsall.bat" x86_amd64
