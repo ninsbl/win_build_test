103a136dbdba2fd27b88f0947fe1b7272d419daa
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-09-19 21:10:49 +0200

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,19 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-which bash
-which sh
-vcdir==$(cygpath "$PROGRAMFILES (x86)\\Microsoft Visual Studio\\2019\\Enterprise")
-echo "$vcdir/VC/Auxiliary/Build/vcvarsall.bat" ${VCARCH:-amd64} $VCSDK
-"$vcdir/VC/Auxiliary/Build/vcvarsall.bat" ${VCARCH:-amd64} $VCSDK
-bat_exe=batch_script
-printf "#!/bin/bash\n/c/osgeo4w/bin/python3 ${bat_exe}.py \$@" > "$bat_exe"
-chmod ugo+x "$bat_exe"
-file "$bat_exe"
-ls "$bat_exe"
-echo $PATH
-echo ""
-echo "########################################"
-echo ""
-echo batch_script -path test
-batch_script -b int=12 path=test
-batch_script -b int=12 path=test > tmp
+vcdir=$(cygpath "C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/dumpbin")
+
+bash $vcdir
