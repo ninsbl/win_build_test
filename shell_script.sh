84822561377569c69fb9021fa5cfdeddebe85985
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-09-19 21:31:09 +0200

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-which dumpbin
-
-dumpbin=$(cygpath -ua "C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/dumpbin.exe")
+dumpbin="$(cygpath -ua \"C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/dumpbin.exe\")"
 
 alias dumpbin="$dumpbin"
 
