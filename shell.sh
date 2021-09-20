3c0ac4895989dc3aa7f2fb4bbd2d57d4cc06a5ee
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-09-20 09:36:15 +0200

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 cd /c/windows
-echo $PATH
+P=$(cygpath -ua "C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/dumpbin.exe")
+echo "$P"
+
+export PATH="${P}:$PATH"
 
 LINK --version
-dumpbin /?
 dumpbin /EXPORTS twain_32.dll out.dll
